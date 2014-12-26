7215877dbae4620d29255cfb1a57e588c338c5c8
andreas wagner <andreaswagnerwi@web.de>
2014-12-26 01:10:23 +0100

    xml

---
+++ XDErrorCode/src/com/daimler/xd/errorcodes/app/data/access/xml/XmlDataFactory.java
@@ -0,0 +1,10 @@
+package com.daimler.xd.errorcodes.app.data.access.xml;
+
+import javax.xml.crypto.Data;
+
+public final class XmlDataFactory {
+	/** */
+	public <T, D extends Data> T createFromData(D data, Class<T> retType) {
+		return null;
+	}
+}
